4a12fbdb51c0499684f3dc2831d08a1b359e7617
e115vk <ve1994@gmail.com>
2019-01-28 11:43:44 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,7 +3,6 @@ def pc_win_move(board, pc_token):
     #выигрышные комбинации
     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
     for each in win_coord:
-        for each in win_coord:
         for i in range(3):
             if board[each[(0+i)%3]] == board[each[(1+i)%3]] == pc_token:
                 if str(board[each[(2+i)%3]]) not in "XO":
